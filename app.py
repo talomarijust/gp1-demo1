2e69ff6a14e70ce9d0ae0ee94262030f4b58dc87
talomarijust <talomari@just.edu.jo>
2025-11-10 16:52:10 +0300

    added  # nosec B104

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def index():
     return render_template_string(HTML_PAGE, result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)  # nosec B104
